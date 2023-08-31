c73708019f294d9117e09355400bab2edfbfcb16
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2023-08-30 23:04:54 -0600

    [IMP] Replace welcome page with login page on base url

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+    ->name('login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
