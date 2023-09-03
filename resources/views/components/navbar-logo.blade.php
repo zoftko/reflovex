c1326a304b982e225b6dbd1d48a8a0ea04969d5e
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2023-09-03 10:43:04 -0600

    [FIX] Navbar logo on light mode not showed fixed

--- resources/views/components/navbar-logo.blade.php
+++ resources/views/components/navbar-logo.blade.php
@@ -1,5 +1,12 @@
+{{-- Light mode --}}
 <img
     alt="Reflovex"
     src="{{asset('logo_dark.png')}}"
     class="hidden dark:block max-w-[50px] rounded-md mx-auto"
+/>
+{{-- Dark mode --}}
+<img
+        alt="Reflovex"
+        src="{{asset('logo.png')}}"
+        class="block dark:hidden max-w-[50px] rounded-md mx-auto"
 />
