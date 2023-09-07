528cf621b2791ce206817cca0bf4c50430bac7d8
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2023-09-06 21:51:29 -0600

    [ADD] Blade service provider for custom directives
    Running in dev mode don't forget run: php artisan view:clear
    And restart your IDE to update blade directives

---
+++ app/Providers/BladeServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class BladeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //Not empty object
+        Blade::if('notEmpty', function ($object) {
+            return ! (empty($object));
+        });
+    }
+}
