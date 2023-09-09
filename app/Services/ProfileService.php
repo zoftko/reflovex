a1b97e15686ffd64d9528c0ce1a8629594fe1c1b
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2023-09-09 12:37:46 -0600

    [ADD] Profiles service class

---
+++ app/Services/ProfileService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Models\Profile;
+
+class ProfileService
+{
+    //Method to retrieve last session
+    public function profilesCount(): int
+    {
+        return Profile::count();
+    }
+}
