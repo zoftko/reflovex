9aafabe1ffc6818c8bcd65df37f8cbb21ff592d2
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2023-09-09 12:35:18 -0600

    [ADD] Boards service class

---
+++ app/Services/BoardService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Models\Board;
+
+class BoardService
+{
+    //Method to retrieve last session
+    public function boardsCount(): int
+    {
+        return Board::count();
+    }
+}
