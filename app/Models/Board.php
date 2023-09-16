5629f939126c7602218521697edb60166784ef82
CrazyBolillo <antonio@zoftko.com>
2023-09-16 10:30:33 -0600

    Add model property docs using ide-helper
    ide-helper was used to autogenerate property documentation for models.
    Some manual changes were applied to conform to phpstan. This enables
    typehinting on models and improves dev experience.

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Board
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $name
+ * @property string|null $ip
+ * @property string|null $last_seen
+ * @property bool $working
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @method static Builder|Board newModelQuery()
+ * @method static Builder|Board newQuery()
+ * @method static Builder|Board query()
+ * @method static Builder|Board whereCreatedAt($value)
+ * @method static Builder|Board whereId($value)
+ * @method static Builder|Board whereIp($value)
+ * @method static Builder|Board whereLastSeen($value)
+ * @method static Builder|Board whereName($value)
+ * @method static Builder|Board whereUpdatedAt($value)
+ * @method static Builder|Board whereUuid($value)
+ * @method static Builder|Board whereWorking($value)
+ *
+ * @mixin Builder<Board>
+ */
 class Board extends Model
 {
     use HasFactory;
--- app/Models/Measurement.php
+++ app/Models/Measurement.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Measurement
+ *
+ * @property int $id
+ * @property int $session_id
+ * @property float $temperature
+ * @property int $sequence
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @method static Builder|Measurement newModelQuery()
+ * @method static Builder|Measurement newQuery()
+ * @method static Builder|Measurement query()
+ * @method static Builder|Measurement whereCreatedAt($value)
+ * @method static Builder|Measurement whereId($value)
+ * @method static Builder|Measurement whereSequence($value)
+ * @method static Builder|Measurement whereSessionId($value)
+ * @method static Builder|Measurement whereTemperature($value)
+ * @method static Builder|Measurement whereUpdatedAt($value)
+ *
+ * @mixin Builder<Measurement>
+ */
 class Measurement extends Model
 {
     use HasFactory;
