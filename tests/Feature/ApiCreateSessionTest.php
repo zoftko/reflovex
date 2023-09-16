e505625b23aea2060e661dc8ab9d0ab51c122d6d
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2023-09-15 23:17:33 -0600

    [ADD] Create api session test

---
+++ tests/Feature/ApiCreateSessionTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Board;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ApiCreateSessionTest extends TestCase
+{
+    protected Board $board;
+   public function setUp(): void
+   {
+       parent::setUp();
+       $this->board = Board::factory()->create([
+       'uuid' => Board::factory()->create()->uuid,
+       'name' => Board::factory()->create()->name
+   ]);
+   }
+
+    public function test_create_session_wrong_body_fields(): void
+    {
+        $response = $this->postJson(route('api.session.store'), [
+            'strangeField' => 'strangeValue',
+            'anotherField' => 'otherValue'
+        ], ['Authorization' => "Basic {$this->board->uuid}:{$this->board->uuid}"]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_create_session_wrong_field_values(): void{
+        $response = $this->postJson(route('api.session.store'), [
+            'uuid' => 2,
+            'soak_temperature' => '10',
+            'soak_time' => 1,
+            'reflow_gradient' => 4,
+            'ramp_up_gradient' => true,
+            'reflow_max_time' => 5,
+            'cooldown_gradient' => -6,
+            'reflow_peak_temp' => '6'
+        ], ['Authorization' => "Basic {$this->board->uuid}:{$this->board->uuid}"]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_create_session(): void{
+        $response = $this->postJson(
+            route('api.session.store'),
+            [
+                'uuid' => $this->board->uuid,
+                'soak_time' => 70,
+                'soak_temperature' => 100,
+                'reflow_gradient' => 2,
+                'reflow_peak_temp' => 183,
+                'reflow_max_time' => 120,
+                'ramp_up_gradient' => 5,
+                'cooldown_gradient' => -6,
+            ],
+            ['Authorization' => "Basic {$this->board->uuid}:{$this->board->uuid}"]
+        );
+
+        $response->assertOk();
+    }
+}
