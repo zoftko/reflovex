5c09b0cb54e0c47bd85eb85538e13051e4eff6ca
Ricardo Rios Soto <ricardo.rios.soto@outlook.com>
2023-09-19 18:08:46 -0600

    [IMP] Remove header from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,11 +4,6 @@
             margin: auto;
         }
     </style>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
     <div class="py-12">
         {{-- Summary boxes --}}
