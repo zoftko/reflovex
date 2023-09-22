28223deadcfece431eefdcdc81826319f1ed8aa8
CrazyBolillo <antonio@zoftko.com>
2023-09-21 19:35:06 -0600

    [REF] Seeder/DB refactoring
    * Fixed error when migrating down for
      _remove_date_from_sessions_table.php.
      Also removed the down migration since
      it will never be used.
    
    * Implemented Model factories to simplify seeding.
    
    * Refactored MeasurementSeeder to take into account the session supposed
      temperature targets.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -13,23 +14,32 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $userRole = Role::firstWhere('name', 'user');
+        $adminRole = Role::firstWhere('name', 'administrator');
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin'),
+            'role_id' => $adminRole->id,
+        ]);
         User::create([
             'name' => 'Francisco Orca',
             'email' => 'francisco.orca@example.com',
             'password' => Hash::make('Fr4nc15c0.0rc423$'),
-            'role_id' => 2,
+            'role_id' => $userRole->id,
         ]);
         User::create([
             'name' => 'Miguel Barajas',
             'email' => 'miguel.barajas@example.com',
             'password' => Hash::make('M1gu3l.b4r4j4523$'),
-            'role_id' => 2,
+            'role_id' => $userRole->id,
         ]);
         User::create([
             'name' => 'Filemon Hurtado Díaz',
             'email' => 'filemon.hurtado1234@example.com',
             'password' => Hash::make('F1l3m0n.hurt4d023$'),
-            'role_id' => 1,
+            'role_id' => $userRole->id,
         ]);
     }
 }
