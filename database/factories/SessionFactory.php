4ddd5d530f793127910cc051ea2f80a53c8d1d13
CrazyBolillo <antonio@zoftko.com>
2023-10-09 23:41:57 -0600

    [REF] remove gradient columns from sessions models
    The kind of poor man's reflow ovens that this app will work with at the
    moment are not capable of precisely controlling their temperature
    gradients, so it makes no sense to have them, its just more JSON payload
    data the potato MCU needs to send over.
    
    BREAKING CHANGE: dropped columns from sessions table.

--- database/factories/SessionFactory.php
+++ database/factories/SessionFactory.php
@@ -20,13 +20,10 @@ class SessionFactory extends Factory
     {
         return [
             'board_id' => Board::factory(),
-            'ramp_up_gradient' => fake()->numberBetween(1, 3),
             'soak_time' => fake()->numberBetween(60, 120),
             'soak_temperature' => fake()->randomElement([90, 100, 110, 115]),
-            'reflow_gradient' => fake()->numberBetween(1, 3),
             'reflow_peak_temp' => fake()->randomElement([145, 150, 160, 170]),
             'reflow_max_time' => fake()->numberBetween(30, 80),
-            'cooldown_gradient' => fake()->numberBetween(1, 5),
         ];
     }
 }
