526498b43e0f816b505fe4db4ffcf551e0fb9e8c
rick-astral-cat <143054530+rick-astral-cat@users.noreply.github.com>
2023-10-17 12:33:50 -0600

    Create reflow artisan command to simulate a reflow session (#30)
    With this reflow command we can simulate a session by setting reflow peak temperature, soak temperature and soak time, but also speed of the simulation in seconds per every database insertion.

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Board;
+use App\Models\Session;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -14,6 +16,66 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
+/*
+ * @var Illuminate\Support\Facades\Artisan $this
+ */
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/*
+ * @var Illuminate\Support\Facades\Artisan $this
+ * @var App\Models\Board $board
+ */
+Artisan::command('reflow {soakTemperature} {soakTime} {reflowPeakTemp} {sleepTime?} {reflowMaxTime?}', function ($soakTemperature, $soakTime, $reflowPeakTemp, $sleepTime = 10, $reflowMaxTime = 80) {
+    $board = Board::inRandomOrder()->limit(1)->first();
+
+    $progress = $this->getOutput()->createProgressBar($soakTime + $reflowPeakTemp + $soakTemperature);
+    $progress->setFormat("%message%\n %current%/%max% [%bar%] %percent:3s%%\n");
+    $progress->setMessage("Creating session on board {$board->name}");
+    $progress->start();
+
+    $session = Session::create([
+        'board_id' => $board->id,
+        'soak_time' => $soakTime,
+        'soak_temperature' => $soakTemperature,
+        'reflow_peak_temp' => $reflowPeakTemp,
+        'reflow_max_time' => $reflowMaxTime,
+    ]);
+
+    $temp = 20;
+    $measurements = collect();
+    $sequence = 1;
+
+    //Make ramp to soak
+    $progress->setMessage('Starting raising to soak temperature...');
+    while ($temp < $soakTemperature) {
+        rcAddMeasurement($measurements, $session->id, $temp, $sequence++);
+        rcSaveMeasurements($measurements, $sleepTime, $progress);
+        $temp += mt_rand(5, 25) / 10;
+    }
+
+    //Now we make soak time
+    $progress->setMessage('Keep on soak time...');
+    for ($seconds = 0; $seconds <= $soakTime; $seconds++, $sequence++) {
+        rcAddMeasurement($measurements, $session->id, $temp, $sequence);
+        rcSaveMeasurements($measurements, $sleepTime, $progress);
+    }
+
+    // Ramp up gradient to peak
+    $progress->setMessage('Ramp up to reflow peak temperature...');
+    while ($temp < $reflowPeakTemp) {
+        rcAddMeasurement($measurements, $session->id, $temp, $sequence++);
+        rcSaveMeasurements($measurements, $sleepTime, $progress);
+        $temp += mt_rand(10, 25) / 10;
+    }
+
+    //Cool down ramp
+    $progress->setMessage('Cooling down...');
+    while ($temp > 50) {
+        rcAddMeasurement($measurements, $session->id, $temp, $sequence++);
+        rcSaveMeasurements($measurements, $sleepTime, $progress);
+        $temp -= mt_rand(20, 40) / 10;
+    }
+    $progress->finish();
+})->purpose('Simulate reflow session');
