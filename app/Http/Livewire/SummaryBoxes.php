895d18a0f435a51698cc65b5b1180365978ba4d9
Ricardo Rios <ricardo.rios.soto@outlook.com>
2023-11-02 11:40:02 -0600

    [ADD] Real time summary boxes

---
+++ app/Http/Livewire/SummaryBoxes.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Board;
+use App\Models\Measurement;
+use App\Models\Profile;
+use App\Models\Session;
+use Carbon\CarbonInterval;
+use Illuminate\Support\Collection;
+use Livewire\Component;
+use Illuminate\View\View;
+
+class SummaryBoxes extends Component
+{
+    public int $boardsCount;
+    public int $sessionsCount;
+    public int $profilesCount;
+    public Collection $statistics;
+
+    public function updateStatistics(): void{
+        $this->statistics = $this->generalStatistics();
+        $this->boardsCount = Board::count();
+        $this->sessionsCount = Session::count();
+        $this->profilesCount = Profile::count();
+    }
+
+    private function generalStatistics(): Collection{
+        $statistics = collect();
+
+        //Max temperature registered
+        $maxTempReg = Measurement::max('temperature');
+        $statistics->put('Max.Temp.Reg.', ['data' => $maxTempReg.' °C', 'class' => 'afterMaxTemp']);
+
+        //Largest session
+        $allMeasurements = Measurement::get()->groupBy('session_id');
+        $max = 0;
+        $sessionKey = null;
+        foreach ($allMeasurements as $sk => $m) {
+            $current = $m->count();
+            if ($current > $max) {
+                $sessionKey = $sk;
+                $max = $current;
+            }
+        }
+        $statistics->put('Longest Session', ['data' => CarbonInterval::seconds($max)->cascade()->forHumans(), 'class' => 'afterLongSession']);
+
+        //Max temperature of the day
+        $maxTempToday = Measurement::whereDay('created_at', (string) (now()->day))->max('temperature');
+        $maxTempToday = $maxTempToday != null ? $maxTempToday.' °C' : 'N/A';
+        $statistics->put('Max.Temp.Today', ['data' => $maxTempToday, 'class' => 'afterTodayTemp']);
+
+        //Board with most sessions
+        $sessions = Session::with('board')->orderBy('id', 'desc')->get()->groupBy('board_id');
+        $boardMostSessions = null;
+        $boardSessionCount = 0;
+        foreach ($sessions as $boardId => $s) {
+            $count = $s->count();
+            if ($count > $boardSessionCount) {
+                $boardSessionCount = $count;
+                $boardMostSessions = $s[0]->board ?? null;
+            }
+        }
+        $boardName = $boardMostSessions->name ?? 'N/A';
+        $statistics->put('Most sessions', ['data' => $boardName.' ('.$boardSessionCount.')', 'class' => 'afterMostSessions']);
+
+        return $statistics;
+    }
+
+    public function render(): view
+    {
+        $this->updateStatistics();
+        return view('livewire.summary-boxes');
+    }
+}
