a99d8c0b0876df366ddcf3fcd1dca635895e9ef4
Ricardo Rios <ricardo.rios.soto@outlook.com>
2023-11-04 12:43:27 -0600

    [IMP] Change last session chart to livewire component and add reload button

---
+++ app/Http/Livewire/LastSession.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Session;
+use Carbon\CarbonInterval;
+use Illuminate\Support\Collection;
+use Illuminate\View\View;
+use Livewire\Component;
+
+class LastSession extends Component
+{
+    /** @mixin Session */
+    public ?Session $session;
+
+    public string $sessionTime;
+
+    /**
+     * @var Collection|array<empty>|null
+     */
+    public $xAxis;
+
+    /**
+     * @var Collection|array<empty>|null
+     */
+    public $yAxis;
+
+    public function updateLastSessionChart(): void
+    {
+        $session = Session::latest()->with('measurements')->first();
+        if (! is_null($session)) {
+            $xAxis = $session->measurements->map(function ($measurement) {
+                return $measurement->sequence;
+            });
+            $yAxis = $session->measurements->map(function ($measurement) {
+                return $measurement->temperature;
+            });
+        }
+        $xAxis = $xAxis ?? [];
+        $yAxis = $yAxis ?? [];
+        $sessionTime = CarbonInterval::seconds(count($yAxis))->cascade()->forHumans();
+        $this->dispatchBrowserEvent('updateLastSessionChartData', [
+            'xdata' => $xAxis,
+            'ydata' => $yAxis,
+            'sessionTime' => $sessionTime,
+        ]);
+    }
+
+    public function render(): view
+    {
+        $this->session = Session::latest()->with('measurements')->first();
+        if (! is_null($this->session)) {
+            $this->xAxis = $this->session->measurements->map(function ($measurement) {
+                return $measurement->sequence;
+            });
+            $this->yAxis = $this->session->measurements->map(function ($measurement) {
+                return $measurement->temperature;
+            });
+        }
+        $this->xAxis = $this->xAxis ?? [];
+        $this->yAxis = $this->yAxis ?? [];
+        $this->sessionTime = CarbonInterval::seconds(count($this->yAxis))->cascade()->forHumans();
+
+        return view('livewire.last-session');
+    }
+}
