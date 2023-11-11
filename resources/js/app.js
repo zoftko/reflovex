f820bf95628ae95b60b7246a7985039f7844b3c8
Ricardo Rios <ricardo.rios.soto@outlook.com>
2023-11-10 19:11:06 -0600

    [ADD] Seewt alert and configuration added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import "./bootstrap";
 import ApexCharts from "apexcharts";
 import Alpine from "alpinejs";
+import Swal from "sweetalert2";
 
+window.Swal = Swal
 window.Alpine = Alpine;
 window.ApexCharts = ApexCharts;
 
